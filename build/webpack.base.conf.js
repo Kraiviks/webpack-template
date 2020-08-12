9b4fd6ddb79711644a9182f29a6f3ba907a982b6
Serhii Kostiv <32710409+kraiviks@users.noreply.github.com>
2020-08-12 18:03:46 +0300

    Update webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -32,7 +32,7 @@ module.exports = {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         loader: "file-loader",
         options: {
-          name: "[name].[ext]"
+          name: "./fonts/[name].[ext]"
         }
       },
       {
@@ -40,7 +40,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: "file-loader",
         options: {
-          name: "[name].[ext]"
+          name: "./img/[name].[ext]"
         }
       },
       {
